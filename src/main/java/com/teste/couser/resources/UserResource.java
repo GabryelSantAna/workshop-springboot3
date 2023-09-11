6c2abff86c0555a2a2bb321b3fdfd5dc908075cc
GabryelSantAna <byels.2002@gmail.com>
2023-09-11 20:17:12 -0300

    User entity and resource

---
+++ src/main/java/com/teste/couser/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.teste.couser.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.couser.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Gabryel","gabryel@gmail.com","123456789","112211");
+		return ResponseEntity.ok().body(u);
+	}
+}
