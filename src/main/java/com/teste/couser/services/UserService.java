af11560a730f88330c851021e790f8406933df21
GabryelSantAna <byels.2002@gmail.com>
2023-09-13 01:36:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/teste/couser/services/UserService.java
@@ -0,0 +1,26 @@
+package com.teste.couser.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teste.couser.entities.User;
+import com.teste.couser.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById (Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
