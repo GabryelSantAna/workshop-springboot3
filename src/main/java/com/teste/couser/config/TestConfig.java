fbe771036064fc3bf3ca03a31b267e3d3c9de25e
GabryelSantAna <byels.2002@gmail.com>
2023-09-15 21:49:57 -0300

    Many-To-Many association with JoinTable

--- src/main/java/com/teste/couser/config/TestConfig.java
+++ src/main/java/com/teste/couser/config/TestConfig.java
@@ -51,6 +51,15 @@ public class TestConfig implements CommandLineRunner{
 		
 		productReoository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productReoository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		User u1 = new User(null,"Gabryel","Gabryel@gmail.com","123456789","11223311");
 		User u2 = new User(null,"Marcos","Marcos@gmail.com","987654321","99886655");
 		
